977e0f4ed4f8f90c25bb245ff0be18734301dd72
samchuk-sv <da_funk_on@mail.ru>
2021-11-26 21:38:22 +0600

    added a daemon

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
+from threading import Thread
 from project import ETL
 from config_db import config_db
 
-if __name__ == '__main__':
-    etl = ETL(config_db)
+
+def start(config):
+    etl = ETL(config)
     etl.main()
-    # etl.pool.close()
+    etl.pool.close()
+
+
+if __name__ == '__main__':
+    stream = Thread(target=start, args=[config_db])
+    stream.start()
